2db10e598d18e40b255c3cc8dbc295216f653655
Samk <sampurnapyne1710@gmail.com>
2026-04-18 12:40:52 +0530

    add worker with channels
    Signed-off-by: Samk <sampurnapyne1710@gmail.com>

---
+++ internal/worker/worker.go
@@ -0,0 +1,30 @@
+package worker
+
+import (
+	"noodle-crawler-go/internal/fetcher"
+	"noodle-crawler-go/internal/parser"
+	"noodle-crawler-go/pkg/models"
+)
+
+func StartWorker(id int, jobs <-chan models.Job, results chan<- models.Result) {
+	for job := range jobs {
+		body, status, err := fetcher.Fetch(job.URL)
+
+		if err != nil {
+			results <- models.Result{
+				URL: job.URL,
+				Err: err,
+			}
+			continue
+		}
+
+		links, err := parser.ExtractLinks(body)
+
+		results <- models.Result{
+			URL:        job.URL,
+			StatusCode: status,
+			Links:      links,
+			Err:        err,
+		}
+	}
+}
